4ffaf41052a91510d1548fcf009d70fcebf8c47a
Brinon <pablo.llueca@gmail.com>
2014-03-19 11:52:09 +0100

    ordering stuff

--- sessio1/ordLlistes.hs
+++ sessio1/ordLlistes.hs
@@ -1,3 +1,4 @@
+--   FUNCIONS AUXILIARS
 -- inserta un element a una llista ordenada
 insert :: [Int] -> Int -> [Int]
 insert [] x = [x]
@@ -24,13 +25,28 @@ merge [] l = l
 merge (x:xs) (y:ys)
   | x < y = [x] ++ merge xs (y:ys)
   | otherwise = [y] ++ merge (x:xs) ys
+                
+-- retorna l'element minim d'una llista
+minList :: [Int] -> Int
+minList [x] = x
+minList (x:xs) 
+  | x < m = x
+  | otherwise = m
+  where m = minList xs
+        
+insertAll :: [Int] -> [Int] -> [Int]
+insertAll [] l = l
+insertAll (x:xs) l = insertAll xs (insert l x)
 
 -- ordenacio x insercio
---isort :: [Int] -> [Int]
-
-
-
-
+isort :: [Int] -> [Int]
+isort l = insertAll l []
 
 --ordenacio x seleccio
---ssort :: [Int] -> [Int]
+ssort :: [Int] -> [Int]
+ssort [x] = [x]
+ssort l = [x] ++ ssort l2
+  where x = minList l
+        l2 = remove l x
+        
+        
