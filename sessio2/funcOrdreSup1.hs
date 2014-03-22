3d5ed3d3eb80e13e91fd4963eac9e52bf3920c40
Brinon <pablo.llueca@gmail.com>
2014-03-22 20:20:36 +0100

    powersOf2

--- sessio2/funcOrdreSup1.hs
+++ sessio2/funcOrdreSup1.hs
@@ -1,3 +1,10 @@
+power :: Int -> Int -> Int
+power x p =
+  if p == 0
+  then 1
+  else x * (power x (p - 1))
+
+
 compt a b = a == b
 
 noF :: [Bool] -> Bool
@@ -10,3 +17,15 @@ eql l1 l2 = noF l3
   where l3 = zipWith compt l1 l2
                  
              
+funcMult :: [Int] -> Int
+funcMult [] = 1
+funcMult (x:xs) = x * funcMult xs
+--prod :: [Int] -> Int
+
+
+powersOf2 :: [Int]
+powersOf2 = [i| j <- [1..], i <- [power 2 j]]
+
+             
+
+
