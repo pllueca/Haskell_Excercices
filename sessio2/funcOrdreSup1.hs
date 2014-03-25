308f910be6870ac1857b13f8f4b354467ed24a0b
Brinon <pablo.llueca@gmail.com>
2014-03-25 09:47:48 +0100

    folding

--- sessio2/funcOrdreSup1.hs
+++ sessio2/funcOrdreSup1.hs
@@ -1,4 +1,4 @@
-power :: Int -> Int -> Int
+--power :: Int -> Int -> Int
 power x p =
   if p == 0
   then 1
@@ -15,17 +15,17 @@ noF (x:xs) = (x /= False) && noF xs
 eql :: [Int] -> [Int] -> Bool
 eql l1 l2 = noF l3
   where l3 = zipWith compt l1 l2
-                 
-             
-funcMult :: [Int] -> Int
-funcMult [] = 1
-funcMult (x:xs) = x * funcMult xs
---prod :: [Int] -> Int
+               
+powersOf2 = iterate (* 2)  1
+
+powersOf x = iterate  (* x) 1
 
 
-powersOf2 :: [Int]
-powersOf2 = [i| j <- [1..], i <- [power 2 j]]
+-- foldl aplica el primer argument a la llista i acumula el
+-- resusltat al segon argument
+prod :: [Int] -> Int
+prod l = foldl (*) 1 l
 
-             
-
-
+prodEvens = foldl (\acc x -> if (mod x 2 == 0) then acc * x else acc) 1
+         
+cont0  = foldl (\acc x -> if x == 0 then acc+1 else acc) 0
