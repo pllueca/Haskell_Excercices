b1758c0203da5b31d6e68f5556df343d72c75ff6
Brinon <pablo.llueca@gmail.com>
2014-03-26 12:42:19 +0100

    llistes working

--- sessio2/llistesComprens.hs
+++ sessio2/llistesComprens.hs
@@ -10,4 +10,7 @@ myZipWith f l1 l2 = [f a b |(a,b) <- zip l1 l2]
 
 
 thingify :: [Int] -> [Int] -> [(Int, Int)]
-thingify l1 l2 = [(a,b) | (a,b) <- ((take 1 l1), (take 1 l2))]
+thingify l1 l2 = [(a,b) | a <- l1, b <- l2, (mod a b) == 0 ]
+
+factors :: Int -> [Int]
+factors x = [y | y <- [1..x], mod x y == 0]
