d6db554dea6e23bd4300a24f435a29d578be4aa0
Brinon <pablo.llueca@gmail.com>
2014-04-03 11:28:46 +0200

    def ordre superio

---
+++ sessio2/defFuncOrdreSup1.hs
@@ -0,0 +1,15 @@
+myFoldl f x [] = x
+myFoldl f x (y:ys) = myFoldl f (f x y) ys
+
+
+myFoldr :: (a -> b -> b) -> b -> [a] -> b
+myFoldr f x [] = x
+myFoldr f x (y:ys) = f y $ myFoldr f x ys
+
+myIterate :: (a -> a) -> a -> [a]
+myIterate f x = (x:ls)
+                where ls = myIterate f (f x)
+                      
+myUntil :: (a -> Bool) -> (a -> a) -> a -> a
+myUntil fb fa x = if fb (fa x) then fa (myUntil fb fa (fa x))
+
