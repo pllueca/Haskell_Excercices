341377e97ca63de5592c7e09bc0b5d1e55c25213
Brinon <pablo.llueca@gmail.com>
2014-04-17 12:44:33 +0200

    index de massa corporal

---
+++ sessio3/masacorporal.hs
@@ -0,0 +1,25 @@
+import System.IO
+
+-- index de massa corporal
+indexMassa :: Float -> String
+indexMassa x
+  | x < 18.0 = "magror"
+  | x >= 18.0 && x < 25.0 = "corpulencia normal"
+  | x >= 25.0 && x < 30.0 = "sobrepes"
+  | x >= 30.0 && x < 40.0 = "obesitat"
+  | otherwise = "obesitat morbida"
+                 
+imc :: Float -> Float -> Float
+imc m h = m / (h * h)
+
+
+--getWord :: IO String 
+getWord = do c <- getChar
+             if (c == '\n') || (c == ' ')
+               then return ""
+               else do w <- getWord
+                       return (c:w)
+                
+--main = do l <- getLine          
+          
+          
